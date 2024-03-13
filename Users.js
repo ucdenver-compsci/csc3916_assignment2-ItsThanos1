b7dc4f3a7a7a3f7274ef94372101f4df3fa11b9c
ItsThanos1 <157194187+ItsThanos1@users.noreply.github.com>
2024-03-13 15:00:34 -0600

    idk4

--- Users.js
+++ Users.js
@@ -2,19 +2,16 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt');
 
-
 mongoose.Promise = global.Promise;
 
-//mongoose.connect(process.env.DB, { useNewUrlParser: true });
+// Connect to MongoDB - Removed deprecated options
 try {
-    mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
-        console.log("connected"));
+    mongoose.connect(process.env.DB, () => console.log("connected"));
 }catch (error) {
-    console.log("could not connect");
+    console.log("could not connect:", error);
 }
-mongoose.set('useCreateIndex', true);
 
-//user schema
+// User schema
 var UserSchema = new Schema({
     name: String,
     username: { type: String, required: true, index: { unique: true }},
@@ -24,25 +21,30 @@ var UserSchema = new Schema({
 UserSchema.pre('save', function(next) {
     var user = this;
 
-    //hash the password
+    // Hash the password
     if (!user.isModified('password')) return next();
 
-    bcrypt.hash(user.password, null, null, function(err, hash) {
+    // bcrypt.hash's first argument is the data to hash, second is the salt to use.
+    // In this case, we're using the genSalt function to generate the salt.
+    bcrypt.genSalt(10, function(err, salt) {
         if (err) return next(err);
 
-        //change the password
-        user.password = hash;
-        next();
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // Change the password to the hashed version
+            user.password = hash;
+            next();
+        });
     });
 });
 
-UserSchema.methods.comparePassword = function (password, callback) {
-    var user = this;
+UserSchema.methods.comparePassword = function(password, callback) {
+    bcrypt.compare(password, this.password, function(err, isMatch) {
+        if (err) return callback(err);
+        callback(null, isMatch);
+    });
+};
 
-    bcrypt.compare(password, user.password, function(err, isMatch) {
-        callback(isMatch);
-    })
-}
-
-//return the model to server
+// Return the model to server
 module.exports = mongoose.model('User', UserSchema);
