54e4f31a1b7f51e4f7441d5968d7fabec9266193
ItsThanos1 <157194187+ItsThanos1@users.noreply.github.com>
2024-03-13 20:46:41 -0600

    what5

--- auth_jwt.js
+++ auth_jwt.js
@@ -1,28 +1,21 @@
 var passport = require('passport');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
+var User = require('./Users');
 
 var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
 opts.secretOrKey = process.env.SECRET_KEY;
 
 passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-    console.log("JWT Payload received:", jwt_payload);
-    User.findById(jwt_payload.id, function (err, user) {
-        if (err) {
-            console.error("Error during user lookup:", err);
-            return done(err, false);
-        }
-        if (user) {
-            console.log("User found:", user);
-            return done(null, user);
-        } else {
-            console.log("No user found with ID from JWT payload:", jwt_payload.id);
-            return done(null, false);
-        }
-    });
-}));
+    var user = db.find(jwt_payload.id);
 
+    if (user) {
+        done(null, user);
+    } else {
+        done(null, false);
+    }
+}));
 
 exports.isAuthenticated = passport.authenticate('jwt', { session : false });
 exports.secret = opts.secretOrKey ;
