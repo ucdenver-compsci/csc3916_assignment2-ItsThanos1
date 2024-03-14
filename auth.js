a2f593d78bb3885c7e51f82092ad55c8d01c48a1
ItsThanos1 <157194187+ItsThanos1@users.noreply.github.com>
2024-03-13 21:59:37 -0600

    k

--- auth.js
+++ auth.js
@@ -3,25 +3,24 @@ var BasicStrategy = require('passport-http').BasicStrategy;
 var User = require('./Users');
 
 passport.use(new BasicStrategy(
-    async function(username, password, done) {
-        try {
-            const user = await User.findOne({ username: username });
-            if (!user) {
-                return done(null, false, { message: 'Incorrect username.' });
-            }
- 
-            // Now using `await` with `comparePassword`
-            const isMatch = await user.comparePassword(password);
-            if (!isMatch) {
-                return done(null, false, { message: 'Incorrect password.' });
-            }
- 
-            return done(null, user);
-        } catch (err) {
-            return done(err);
-        }
-    }
- ));
+   async function(username, password, done) {
+       try {
+           const user = await User.findOne({ username: username });
+           if (!user) {
+               return done(null, false, { message: 'Incorrect username.' });
+           }
+
+           const isMatch = await user.comparePassword(password);
+           if (isMatch) {
+               return done(null, user);
+           } else {
+               return done(null, false, { message: 'Incorrect password.' });
+           }
+       } catch (err) {
+           return done(err);
+       }
+   }
+));
 
 
 exports.isAuthenticated = passport.authenticate('basic', { session: false });
